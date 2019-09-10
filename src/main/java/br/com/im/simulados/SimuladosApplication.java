6337ab8935cd4fee01ee65b494e0f5530083d3b7
igor.m.peixoto <igor.m.peixoto@gmail.com>
2019-09-10 19:10:21 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/im/simulados/SimuladosApplication.java
@@ -0,0 +1,13 @@
+package br.com.im.simulados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladosApplication.class, args);
+	}
+
+}
